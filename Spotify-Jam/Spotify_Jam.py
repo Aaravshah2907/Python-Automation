0c2634527d68fe993e44316bf9de18c90ea1d9d3
Aarav <aaravshah0011@gmail.com>
2025-01-29 23:51:49 +0530

    Updating some parts of the script for easy referral.

--- Spotify-Jam/Spotify_Jam.py
+++ Spotify-Jam/Spotify_Jam.py
@@ -1,30 +1,30 @@
-import pyautogui
+import pyautogui as pg
 import time
 
 # Give yourself a few seconds to switch to the desktop
 for x in range(0, 1):
-    pyautogui.hotkey("command", "space")
+    pg.hotkey("command", "space")
     time.sleep(1.5)
 
 # Open Spotify (assuming it's pinned to the taskbar or you can use a keyboard shortcut)
-pyautogui.hotkey("command", "space")
+pg.hotkey("command", "space")
 time.sleep(0.5)
-pyautogui.write('Spotify.app')  
+pg.write('Spotify.app')  
 time.sleep(0.5)
-pyautogui.press('enter')
+pg.press('enter')
 
 # Wait for Spotify to open
 time.sleep(5)
 
 # Maximize the Spotify window
-pyautogui.hotkey('control','command', 'f')
+pg.hotkey('control','command', 'f')
 
 #Locating the connect to device option on spotify
-location = pyautogui.click (1250,912)
+pg.click (1250,912)
 time.sleep(1)
 
 #Locating the create a jam option on spotify
-location = pyautogui.click (1267,278)
+pg.click (1267,278)
 time.sleep(1)
 
 # Locating the create a jam option on spotify
@@ -32,35 +32,35 @@ time.sleep(1)
 # time.sleep(1.2) 
 
 # Open Whatsapp in full screen
-pyautogui.hotkey("command", "space")
+pg.hotkey("command", "space")
 time.sleep(1)
-pyautogui.write('whatsapp')  
+pg.write('whatsapp')  
 time.sleep(1)
-pyautogui.press('enter')
+pg.press('enter')
 time.sleep(3)
-pyautogui.hotkey('fn', 'f')
+pg.hotkey('fn', 'f')
 time.sleep(1)
 
 # Click on the chat boxCLICK ON SEARCH BUTTON
-location = pyautogui.click(204,107)
+pg.click(204,107)
 time.sleep(0.5)
 
 # Search Jainam Shah
-pyautogui.write('Jainam Shah')
+pg.write('Jainam Shah')
 time.sleep(0.5)
 
 # Click on first option
-location = pyautogui.click(224,215)
+pg.click(224,215)
 time.sleep(0.5)
 
 # Click on Searchbar
-location = pyautogui.click(653,932)
+pg.click(653,932)
 time.sleep(0.5)
 
 # Paste the code
-pyautogui.hotkey("command", "v")
+pg.hotkey("command", "v")
 time.sleep(0.5)
 
 # Click Enter
-pyautogui.hotkey("return")
+pg.hotkey("return")
 time.sleep(0.5)
